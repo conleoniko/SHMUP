f787d9a805216ea9bacf72db5877bec9ea522420
conleoniko <lnikonowicz@yahoo.com>
2020-10-11 17:59:41 -0500

    Finished the hero

---
+++ Space SHMUP Prototype/Assets/__Scripts/BoundsCheck.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float radius = 1f;
+
+    [Header("Set Dynamically")]
+    public float camWidth;
+    public float camHeight;
+
+
+    void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if ( pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+        }
+        if ( pos.x < -camWidth + radius)
+        {
+            pos.x = -camWidth + radius;
+        }
+        if ( pos.y > camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+        }
+        if ( pos.y < -camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+        }
+        transform.position = pos;
+
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, 0.1f);
+        Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Space SHMUP Prototype/Assets/__Scripts/Hero.cs
+++ Space SHMUP Prototype/Assets/__Scripts/Hero.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
+    static public Hero S;
+
+
+    [Header("Set in Inspector")]
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +37,14 @@ public class Hero : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
     }
 }
