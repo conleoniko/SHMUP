5bf3900e5c5c2a5cb0e3fed03ca2f7c6e2a43eca
conleoniko <lnikonowicz@yahoo.com>
2020-10-14 17:38:06 -0500

    finished scrolling screen
    finished scrolling screen

---
+++ Space SHMUP Prototype/Assets/__Scripts/Parallax.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject poi;
+    public GameObject[] panels;
+    public float scrollSpeed = -30f;
+
+    public float motionMult = .25f;
+
+    private float panelHt;
+    private float depth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelHt = panels[0].transform.localScale.y;
+        depth = panels[0].transform.position.z;
+
+        panels[0].transform.position = new Vector3(0, 0, depth);
+        panels[0].transform.position = new Vector3(0, panelHt, depth);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tY, tX = 0;
+        tY = Time.time * scrollSpeed % panelHt + (panelHt * 0.5f);
+
+        if(poi != null)
+        {
+            tX = -poi.transform.position.x * motionMult;
+        }
+        panels[0].transform.position = new Vector3(tX, tY, depth);
+
+        if(tY >= 0)
+        {
+            panels[1].transform.position = new Vector3(tX, tY - panelHt, depth);
+        }
+        else
+        {
+            panels[1].transform.position = new Vector3(tX, tY - panelHt, depth);
+        }
+    }
+}
